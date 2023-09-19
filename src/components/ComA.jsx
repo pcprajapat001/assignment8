fa1eb2d85e2c4c049ef7acd7e849465311cdb373
PC_Prajapat <contact@pcprajapat.com>
2023-09-19 21:36:07 +0530

    cc

--- src/components/ComA.jsx
+++ src/components/ComA.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 export const ComA= ({text,text2,id})=>{
     return(
-        <div id={id} className="sub1">
+        <div  className="sub1">
             <div>
-            <h3 id={id}>{text}</h3>
+            <h3 >{text}</h3>
             
-            <h3 id={id}>{text2}</h3>
+            <h3 >{text2}</h3>
 
             </div>
         </div>
